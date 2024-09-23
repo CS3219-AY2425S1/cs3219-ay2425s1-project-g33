ae4b9546ce112a82a1aec33a9ea567a359b17289
Rick <rick.kohjiaxuan@gmail.com>
2024-09-23 17:39:28 +0800

    Add async function to fetch questions from the server

--- frontend/src/services/questionService.ts
+++ frontend/src/services/questionService.ts
@@ -1,1 +1,24 @@
-// export your fetchers here
+"use server";
+
+import { Questions, QuestionsArraySchema } from "@/types/Question";
+
+export default async function getQuestions(): Promise<Questions> {
+  try {
+    const res = await fetch(
+      process.env.PUBLIC_API_URL + "/question/questions",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await res.json();
+
+    return QuestionsArraySchema.parse(data);
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
