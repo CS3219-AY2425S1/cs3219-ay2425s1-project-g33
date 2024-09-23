2f32ab44e29baa16a64c2ef1603d4a510ce97710
Rick <rick.kohjiaxuan@gmail.com>
2024-09-23 17:38:24 +0800

    Define Question type

--- frontend/src/types/Question.ts
+++ frontend/src/types/Question.ts
@@ -1,4 +1,18 @@
-export type Question = {
-  id: string;
-  title: string;
-};
+import { z } from "zod";
+
+export const QuestionSchema = z.object({
+  supportedLanguages: z.array(z.string()),
+  _id: z.string(),
+  title: z.string(),
+  questionNumber: z.number(),
+  slug: z.string(),
+  description: z.string(),
+  difficulty: z.enum(["Easy", "Medium", "Hard"]),
+  categories: z.array(z.string()),
+});
+
+export const QuestionsArraySchema = z.array(QuestionSchema);
+
+export type Question = z.infer<typeof QuestionSchema>;
+
+export type Questions = z.infer<typeof QuestionsArraySchema>;
