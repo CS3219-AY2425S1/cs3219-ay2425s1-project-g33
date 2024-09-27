e6ed3150a6828a24b6dcf152283ddde7e703b45a
Rick <rick.kohjiaxuan@gmail.com>
2024-09-27 14:27:26 +0800

    QuestionTable simple error handling

--- frontend/src/app/dashboard/_components/QuestionTable/QuestionTable.tsx
+++ frontend/src/app/dashboard/_components/QuestionTable/QuestionTable.tsx
@@ -1,22 +1,23 @@
-import getQuestions from "@/services/questionService";
-import { Questions } from "@/types/Question";
+import { getQuestions } from "@/services/questionService";
+import { QuestionsResponse } from "@/types/Question";
 import { questionTableColumns } from "./column";
 import { DataTable } from "./data-table";
 
 export default async function QuestionTable() {
-  const questions: Questions = await getQuestions();
+  const questionsResponse: QuestionsResponse = await getQuestions();
 
-  const categories: string[] = questions
-    .map((question) => question.categories)
-    .flat()
-    .filter((category, index, self) => self.indexOf(category) === index)
-    .sort();
+  if (!questionsResponse.data) {
+    return <div>{questionsResponse.message}</div>;
+  }
+
+  const questions = questionsResponse.data.questions;
+  const totalCategories = questionsResponse.data.totalCategories;
 
   return (
     <DataTable
       columns={questionTableColumns}
       data={questions}
-      categories={categories}
+      categories={totalCategories}
     />
   );
 }
