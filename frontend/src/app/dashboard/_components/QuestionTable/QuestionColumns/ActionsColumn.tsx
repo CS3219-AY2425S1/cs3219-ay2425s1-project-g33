4aa2b235e7eaf60483b021eba1316fa060907110
Rick <rick.kohjiaxuan@gmail.com>
2024-09-28 16:27:31 +0800

    Fix eslint errors

--- frontend/src/app/dashboard/_components/QuestionTable/QuestionColumns/ActionsColumn.tsx
+++ frontend/src/app/dashboard/_components/QuestionTable/QuestionColumns/ActionsColumn.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { Question, QuestionSchema } from "@/types/Question";
-import { ColumnDef } from "@tanstack/react-table";
+import { ColumnDef, Row } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -15,46 +17,48 @@ import DeleteQuestionDialog from "../../Forms/DeleteQuestionDialog";
 const ActionsColumn: ColumnDef<Question> = {
   id: "Action",
   header: () => <div className="w-24 px-4 text-right">Actions</div>,
-  cell: ({ row }) => {
-    const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
-    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-    const question = QuestionSchema.parse(row.original);
-
-    return (
-      <div className="px-4 text-right">
-        <DeleteQuestionDialog
-          isOpen={isDeleteDialogOpen}
-          setIsOpen={setIsDeleteDialogOpen}
-          question={question}
-        />
-        <EditQuestionModal
-          isOpen={isEditModalOpen}
-          setIsOpen={setIsEditModalOpen}
-          question={question}
-        />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon">
-              <MoreHorizontal className="w-4 h-4" />
-              <span className="sr-only">More</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem
-              onClick={() => {
-                setIsEditModalOpen(true);
-              }}
-            >
-              Edit
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setIsDeleteDialogOpen(true)}>
-              Delete
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-    );
-  },
+  cell: ({ row }) => ActionsColumnCell({ row }),
 };
+
+function ActionsColumnCell({ row }: { row: Row<Question> }) {
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const question = QuestionSchema.parse(row.original);
+
+  return (
+    <div className="px-4 text-right">
+      <DeleteQuestionDialog
+        isOpen={isDeleteDialogOpen}
+        setIsOpen={setIsDeleteDialogOpen}
+        question={question}
+      />
+      <EditQuestionModal
+        isOpen={isEditModalOpen}
+        setIsOpen={setIsEditModalOpen}
+        question={question}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon">
+            <MoreHorizontal className="w-4 h-4" />
+            <span className="sr-only">More</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem
+            onClick={() => {
+              setIsEditModalOpen(true);
+            }}
+          >
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setIsDeleteDialogOpen(true)}>
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
 
 export default ActionsColumn;
